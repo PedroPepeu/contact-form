141d0ee0c9e18ff081694c0bf6aa202d17d8d338
PedroPepeu <pedrosteamgtav@gmail.com>
2024-04-16 08:48:19 -0300

    test

---
+++ src/script.js
@@ -0,0 +1,45 @@
+const form = document.querySelector('#frm');
+const inpName = document.getElementById('name');
+const inpEmail = document.getElementById('email');
+const inpMsg = document.getElementById('msg');
+
+const urlBase = "https://parseapi.back4app.com/classes/Chamado";
+
+const headers = {
+    "X-Parse-Applications-Id": "Zk8NxNRBKdm57eMriIewhCfuoghpWRyeZEPQzj1p",
+    "X-Parse-REST-API-Key": "FaZAEmmGgjIIY8TPztDcAiMkghsUY0AMFTbHLJgK",
+};
+
+const headersJson = {
+    ...headers,
+    "Content-Type": "application/json",
+};
+
+const addMsg = async(evt) => {
+    evt.preventDefault();
+    try {
+        let msgInfo = {
+            name: inpName.value,
+            email: inpEmail.value,
+            msg: inpMsg.value,
+        }
+    
+        if(!msgInfo.name || msgInfo.name.trim() === "") {
+            alert("Digite um nome");
+            return;
+        }
+    
+        const msgCreate = await fetch(urlBase, {
+            method: "POST",
+            headers: headersJson,
+            body: JSON.stringify(msgInfo),
+        });
+    
+        const data = await msgCreate.json();
+        console.log("data", data);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+form.addEventListener('submit', addMsg);
